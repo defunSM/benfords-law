373a4adc20100f5f0520ca52fa380ff584e915f6
defunsm <sxh8083@g.rit.edu>
2017-05-31 23:00:10 -0400

    new

--- benford.py
+++ benford.py
@@ -5,8 +5,32 @@ import requests
 from bs4 import BeautifulSoup
 from optparse import OptionParser
 
+
+## Using benford's law to detect fraud or misinformation in articles.
+
 def main():
-    pass
+
+    parser = OptionParser()
+    parser.add_option("-l", "--link", help="Select a link to scrap.")
+
+    options, arguments = parser.parse_args()
+
+    if options.link:
+        link = options.link
+        resp = requests.get(link)
+        soup = BeautifulSoup(resp.text, 'lxml')
+
+        f = open('webfile.txt', 'w')
+        f.write(soup.txt)
+
+        f.close()
+        print(link, "[Completed]")
+
+
+
+    else:
+        print("No link given using -l.")
+
 
 if __name__ == "__main__":
     main()
